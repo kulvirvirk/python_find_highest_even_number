d90b6c808f384d1014d2988471ac0d860e5899b4
kulvirvirk <kvirkdev@gmail.com>
2020-03-12 05:42:46 +0000

    added instructor's solution example

--- main.py
+++ main.py
@@ -8,3 +8,21 @@ def highest_even(li):
                 highest_even_number = number
     return highest_even_number
 print(highest_even([10, 2, 30, 44, 81]))
+
+
+# instructor's solution
+
+
+#====================================================
+# instructor's solution 
+
+# def highest_even(li):
+#   #create a empty list called evens []
+#   evens = []
+#   #iterate through orginal is to separate only even numbers
+#   for item in li:
+#     if item % 2 == 0:
+#       #add all the even numbers to new evens list
+#       evens.append(item)
+#   #use max() function, which returns a highest number    
+#   return max(evens)
