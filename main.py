cf9d823cf6ecd8dcb382f77f8723627c7f0f3d14
kulvirvirk <kvirkdev@gmail.com>
2020-03-12 05:34:32 +0000

    Created the function that iterates through a list to find the highest number

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 # 1. create a highest_even function that prints a highest envn number from a given list
+
+def highest_even(li):
+    highest_even_number = 0
+    for number in li:
+        if number % 2 == 0:
+            if highest_even_number < number:
+                highest_even_number = number
+    return highest_even_number
+print(highest_even([10, 2, 30, 44, 81]))
